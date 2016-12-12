f95a81a058c4638432a8215f705510ca93e0d875
ilovelili <route666@live.cn>
2016-12-12 18:12:58 +0900

    remove export feature

--- dcome/query/orderquery.go
+++ dcome/query/orderquery.go
@@ -3,7 +3,7 @@ package query
 import "fmt"
 
 // SQLOrderQuery query returns order info
-func SQLOrderQuery() string {
+func SQLOrderQuery(date string) string {
 	return fmt.Sprintf(
 		`select 
 		items.item_id, 
@@ -20,15 +20,5 @@ func SQLOrderQuery() string {
 		FROM lms.line_items items 
 		join lms.orders orders on orders.id = items.order_id 
 		join lms.addresses addresses on addresses.id = items.address_id 
-		join lms.skus skus on skus.id = items.sku_id where items.line_status_id != 1`)
-}
-
-// BigQuerySkuQuery Sku analytics query
-func BigQuerySkuQuery() string {
-	return fmt.Sprintf(`select mcpskuname, DATE(TIMESTAMP(created)) as created, quantity, name, city from lms.orders group by mcpskuname, created, quantity, name, city order by created desc, quantity desc`)
-}
-
-// BigQueryDailyQuery Daily order analytics query
-func BigQueryDailyQuery() string {
-	return fmt.Sprintf(`select DATE(TIMESTAMP(created)) as created, mcpskuname, quantity, name, city from lms.orders group by created, mcpskuname, quantity, name, city order by created desc, quantity desc`)
+		join lms.skus skus on skus.id = items.sku_id where items.line_status_id != 1 and DATE(created_at)=%s`, date)
 }
--- etl/main.go
+++ etl/main.go
@@ -7,42 +7,26 @@ import (
 	"etl/config"
 	"etl/pipeline"
 	"flag"
-	"fmt"
 	"os"
 	"time"
 
-	"github.com/dailyburn/ratchet"
 	"github.com/dailyburn/ratchet/processors"
 	_ "github.com/denisenkom/go-mssqldb"
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
-	"golang.org/x/text/encoding/japanese"
 )
 
 var (
-	mode = flag.String("mode", "", "define run mode")
+	date = flag.String("date", "", "object date")
 )
 
 func main() {
 	flag.Parse()
 
-	// export data to cloud
-	if *mode == "export" {
-		dump()
-		os.Exit(0)
+	if *date == "" {
+		*date = time.Now().Format("2006-01-02")
 	}
 
-	if *mode == "import" {
-		read()
-		os.Exit(0)
-	}
-
-	fmt.Println("Mode unassigned. Exit")
-	os.Exit(0)
-}
-
-// on-premised to cloud
-func dump() {
 	config := config.GetConfig()
 	db, err := sql.Open(config.Client, config.GetConnectionString())
 	defer db.Close()
@@ -51,7 +35,7 @@ func dump() {
 		panic(err.Error())
 	}
 
-	orders := processors.NewSQLReader(db, query.SQLOrderQuery())
+	orders := processors.NewSQLReader(db, query.SQLOrderQuery(*date))
 	transformer := transformer.NewOrderTransformer()
 
 	bigqueryconfig := &processors.BigQueryConfig{JsonPemPath: config.JsonPemPath, ProjectID: config.ProjectID, DatasetID: config.DatasetID}
@@ -63,48 +47,6 @@ func dump() {
 		panic(err.Error())
 	}
 
-	fmt.Println(<-pipeline.Run())
-}
-
-// cloud to on-premises
-func read() {
-	config := config.GetConfig()
-
-	bigqueryconfig := &processors.BigQueryConfig{JsonPemPath: config.JsonPemPath, ProjectID: config.ProjectID, DatasetID: config.DatasetID}
-	bigquerysku := processors.NewBigQueryReader(bigqueryconfig, query.BigQuerySkuQuery())
-	bigquerydaily := processors.NewBigQueryReader(bigqueryconfig, query.BigQueryDailyQuery())
-
-	// use shift_jis
-	encoder := japanese.ShiftJIS.NewEncoder()
-	sku, _ := os.Create(config.CSVSavePath + "sku" + time.Now().Format("20060102") + ".csv")
-	daily, _ := os.Create(config.CSVSavePath + "daily" + time.Now().Format("20060102") + ".csv")
-	defer sku.Close()
-	defer daily.Close()
-
-	writeSkuCSV := processors.NewCSVWriter(encoder.Writer(sku))
-	writeDailyCSV := processors.NewCSVWriter(encoder.Writer(daily))
-
-	layout, err := ratchet.NewPipelineLayout(
-		ratchet.NewPipelineStage(
-			ratchet.Do(bigquerysku).Outputs(writeSkuCSV),
-			ratchet.Do(bigquerydaily).Outputs(writeDailyCSV),
-		),
-		ratchet.NewPipelineStage(
-			ratchet.Do(writeSkuCSV),
-			ratchet.Do(writeDailyCSV),
-		),
-	)
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	// Finally, create and run the Pipeline
-	pipeline := ratchet.NewBranchingPipeline(layout)
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	fmt.Println(<-pipeline.Run())
+	<-pipeline.Run()
+	os.Exit(0)
 }
