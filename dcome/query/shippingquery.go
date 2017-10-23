1f3c11b0f4f17204cba1073b6dcf35f4d0487c22
mju <mju@cimpress.com>
2017-10-23 17:34:16 +0900

    fix query

--- dcome/query/shippingquery.go
+++ dcome/query/shippingquery.go
@@ -4,5 +4,5 @@ import "fmt"
 
 // SQLShippingQuery query returns shipping info
 func SQLShippingQuery(from string, to string) string {
-	return fmt.Sprintf(`SELECT * FROM lms.trackings where created_at between '%s' and '%s'`, from, to)
+	return fmt.Sprintf(`SELECT * FROM lms.trackings where created_at between '%s' and '%s';`, from, to)
 }
